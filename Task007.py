df5a9cbda7d9d8a76f3402ec04861f4f4ed0e0ab
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-04-20 21:40:08 +0300

    Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат

--- Task007.py
+++ Task007.py
@@ -18,3 +18,26 @@ if (check == 1):
     print('Выражение ИСТИННО!')
 else:
     print('Выражение ЛОЖНО!')
+
+
+"""def logic(x, y, z):
+    if (not (x or y or z) == (not x and not y and not z)):
+        result = True
+    else:
+        result = False
+        print(result)
+    return result
+
+f1 = logic(True, True, True)
+f2 = logic(True, True, False)
+f3 = logic(True, False, True)
+f4 = logic(False, True, True)
+f5 = logic(True, False, False)
+f6 = logic(False, False, True)
+f7 = logic(False, True, True)
+f8 = logic(False, False, False)
+
+if (all([f1, f2, f3, f4, f5, f6, f7, f8])) == True:
+    print('Выражение тождественно')
+else:
+    print('Выражение НЕ тождественно')"""
