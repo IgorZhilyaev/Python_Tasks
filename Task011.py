21b2600d8b170cdb87f5f3359df4884cbd4fafd0
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-05-23 19:04:11 +0300

    Сформировать список из N членов последовательности. new

--- Task011.py
+++ Task011.py
@@ -1,15 +1,6 @@
 #Сформировать список из N членов последовательности.
 #Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-list = []
-N = int(input('Введите количество членов последовательности: '))
-for i in range(N):
-    if i % 2 == 0:
-        list.append(3**i)
-    else:
-        list.append(-3**i)
-print(list)
-
 """
 def power(s):
 
@@ -27,3 +18,15 @@ for i in range(n):
 
 print(f"List of elements: {spisok}")
 """
+
+#list = []
+#N = int(input('Введите количество членов последовательности: '))
+#for i in range(N):
+#    if i % 2 == 0:
+#        list.append(3**i)
+#    else:
+#        list.append(-3**i)
+
+list = [(-3) ** i for i in range (int(input('Введите количество членов последовательности: ')))]
+
+print(list)
