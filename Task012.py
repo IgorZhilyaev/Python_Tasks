1ef75c0be80ef602b926c1761aa3d3606d09d199
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-05-23 19:11:46 +0300

    Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1. new

--- Task012.py
+++ Task012.py
@@ -6,12 +6,15 @@ import os
 os.system("cls")
 
 
-dictionary = {n:3*n+1 for n in range (1,7)}
-print (dictionary)
+#dictionary = {n:3*n+1 for n in range (1,7)}
+#print (dictionary)
 
 
-dictionary = {}
-n = int(input('n = '))
-for i in range(1,n+1):
-    dictionary[i] = 3*i+1
+#dictionary = {}
+#n = int(input('n = '))
+#for i in range(1,n+1):
+#    dictionary[i] = 3*i+1
+#print(dictionary)
+
+dictionary = {x: 3 * x + 1 for x in range(1, int(input('Задайте количество элементов последовательности: '))+1)}
 print(dictionary)
