aa5e407c667c32b3641042946e837de3b1e50bb0
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-05-23 19:37:15 +0300

    Задать список из n чисел последовательности (1+1n)n и вывести на экран их сумму. new

--- Task016.py
+++ Task016.py
@@ -1,14 +1,17 @@
 # Задать список из n чисел последовательности (1+1n)n и вывести на экран их сумму.
 
 import os
-import random
 os.system("cls")
 
 n = int(input('Введите число N: '))
 
-massive = []
+#massive = []
 
-for i in range(1, n+1):
-    massive.append(1+(1/i)**i)
-print(f'Полученный массив:\n{massive}\n')
-print(f'Сумма чисел массива:\n{sum(massive):.2f}')
+#for i in range(1, n+1):
+#    massive.append(1+(1/i)**i)
+#print(f'Полученный массив:\n{massive}\n')
+#print(f'Сумма чисел массива:\n{sum(massive):.2f}')
+
+num = [1+(1/i)**i for i in range(1, n+1)]
+print('Задана последовательность из ', n,
+'чисел:\n', *num, '\nСумма чисел = ', round(sum(num), 2), '\n')
