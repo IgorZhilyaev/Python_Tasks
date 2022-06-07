14cd7832b2237a9d5e4cb220ab294f73e9ac2af3
Igor Zhilyaev <igor-zhilyaev89@yandex.ru>
2022-06-07 10:38:00 +0300

    all

---
+++ Sem_07/calk_complex.py
@@ -0,0 +1,12 @@
+
+def calc(a:complex, b:complex, operation):
+    a = complex(a)
+    b = complex(b)
+    if operation == '+':
+        return a+b
+    if operation == '-':
+        return a-b
+    if operation == '*':
+        return a*b
+    if operation == '/':
+        return a/b
